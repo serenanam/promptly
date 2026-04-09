cc8907d5627e47f5d3d80ad339182e7048d80612
serenanam <73319962+serenanam@users.noreply.github.com>
2026-04-09 17:41:43 -0400

    add gmail auth connection

---
+++ src/auth.py
@@ -0,0 +1,53 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+token_path = os.path.join(BASE_DIR, "token.json")
+creds_path = os.path.join(BASE_DIR, "credentials.json")
+
+def connect_gmail_auth():
+    creds = None
+    # get path if already exists
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    
+    # let user log in if no credentials found
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        # Save the credentials for the next run
+        with open(token_path, "w") as token:
+            token.write(creds.to_json())
+    
+    try:
+        # Call the Gmail API
+        service = build("gmail", "v1", credentials=creds)
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
+
+        if not labels:
+            print("No labels found.")
+            return
+        
+        print("Labels:")
+        for label in labels:
+            print(label["name"])
+            
+        return service
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f"An error occurred: {error}")
+    
+    
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from auth import connect_gmail_auth
+
+service = connect_gmail_auth()
+print(service)
